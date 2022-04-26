32fb5849272a3a79b1b27dfb668f0e96530b7d3f
Vlads <vladsbg@gmail.com>
2022-04-26 14:13:12 -0300

    clone project

---
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -0,0 +1,45 @@
+import Order from "../../../../domain/checkout/entity/order";
+import OrderItemModel from "./order-item.model";
+import OrderModel from "./order.model";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
+
+export default class OrderRepository implements OrderRepositoryInterface{
+  async create(entity: Order): Promise<void> {
+    await OrderModel.create(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        include: [{ model: OrderItemModel }],
+      }
+    );
+  }
+
+  async update(entity: Order): Promise<void> {
+      
+  }
+
+  async find(id: string): Promise<Order> {
+      const orderModel = await OrderModel.findOne({ where: { id } });
+      return new Order(orderModel.id, orderModel.customer_id, orderModel.items.map(item => new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity)));
+  }
+
+  async findAll(): Promise<Order[]> {
+    const orderModels = await OrderModel.findAll();
+    return orderModels.map(order => {
+      return new Order(order.id, order.customer_id, order.items.map(item => {
+        return new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity);
+      }));
+    });
+  }
+}
