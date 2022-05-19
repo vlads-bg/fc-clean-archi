3d6a5d99901dbe71f5f061212697dac12ac745f0
Vlads <vladsbg@gmail.com>
2022-05-18 22:38:30 -0300

    feat: product / notification pattern

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,15 +1,20 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
 import ProductInterface from "./product.interface";
 
-export default class ProductB implements ProductInterface {
-  private _id: string;
+export default class ProductB extends Entity implements ProductInterface {
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   get id(): string {
@@ -34,16 +39,15 @@ export default class ProductB implements ProductInterface {
     this.validate();
   }
 
-  validate(): boolean {
+  validate() {
     if (this._id.length === 0) {
-      throw new Error("Id is required");
+      this.notification.addError({ message: "Id is required", context: "product"});
     }
     if (this._name.length === 0) {
-      throw new Error("Name is required");
+      this.notification.addError({ message: "Name is required", context: "product"});
     }
     if (this._price < 0) {
-      throw new Error("Price must be greater than zero");
+      this.notification.addError({ message: "Price must be greater than zero", context: "product"});
     }
-    return true;
   }
 }
