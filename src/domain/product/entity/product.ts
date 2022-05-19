3309848a6e59b4ca62e80d87efa601eea0e4811b
Vlads <vladsbg@gmail.com>
2022-05-19 20:45:04 -0300

    feat: product / validator factory / yup

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
@@ -40,14 +41,6 @@ export default class Product extends Entity implements ProductInterface {
   }
 
   validate() {
-    if (this._id.length === 0) {
-      this.notification.addError({ message: "Id is required", context: "product"});
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({ message: "Name is required", context: "product"});
-    }
-    if (this._price < 0) {
-      this.notification.addError({ message: "Price must be greater than zero", context: "product"});
-    }
+    ProductValidatorFactory.create().validate(this);
   }
 }
